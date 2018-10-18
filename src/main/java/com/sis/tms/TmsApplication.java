1972a4cfb639d3dff2d282f0539ff3f1aa50bd36
babukondalraj <42045427+babukondalraj@users.noreply.github.com>
2018-10-17 22:17:44 -0400

    Testing direct commit in git hub

--- src/main/java/com/sis/tms/TmsApplication.java
+++ src/main/java/com/sis/tms/TmsApplication.java
@@ -2,7 +2,7 @@ package com.sis.tms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Testing 
 @SpringBootApplication
 public class TmsApplication {
 
